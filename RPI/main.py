c93e6d53221519070fcf79e0610ef599b280a9c1
Auri O'Malley <37745048+auriomalley@users.noreply.github.com>
2019-12-10 21:00:38 +0300

    Removed PYTHONPATH

--- RPI/main.py
+++ RPI/main.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(Path(os.getcwd()).joinpath("../modules"))
+sys.path.append(str(Path(os.getcwd()).parents[0].joinpath("modules")))
 
 from enum import Enum, auto
 
