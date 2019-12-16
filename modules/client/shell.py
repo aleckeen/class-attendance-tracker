4c2123b3ca3a41eb08baf1eec82613ad10d4c1b6
Auri O'Malley <37745048+auriomalley@users.noreply.github.com>
2019-12-17 01:41:46 +0300

    Added help messages for commands

--- modules/client/shell.py
+++ modules/client/shell.py
@@ -1,37 +1,42 @@
-from pprint import pprint
-
-import pymongo
-from typing import Tuple, Optional
-
 from modules.client import database, data, commands, connection
 from modules.client.utils import print_table, show_image
 
 import cmd
 
 
-def parse(line: str) -> Tuple[Optional[str], Optional[str]]:
-    args = line.strip().split()
-    if len(args) % 2 == 0:
-        return args[-1], None
-
-
 class Shell(cmd.Cmd):
     prompt = " >> "
-    intro = "Class Attendance Tracker Client\n" \
-            "https://github.com/auriomalley/class-attendance-tracker"
+    pre_loop_intro = "Class Attendance Tracker Client\n" \
+                     "https://github.com/auriomalley/class-attendance-tracker\n" \
+                     "Still in development. May crash frequently.\n" \
+                     "^D to exit or simply type: exit"
+    doc_header = "A list of available commands. (type: help <command>)"
 
     def default(self, line):
         if line == "EOF":
             print("Bye")
             return True
 
+    def preloop(self):
+        if self.pre_loop_intro:
+            self.stdout.write(str(self.pre_loop_intro) + "\n")
+        self.onecmd("help")
+
+    def emptyline(self):
+        return
+
     @staticmethod
     def do_exit(arg):
+        """Exit the application.
+        Usage: exit (Exits the application.)"""
         print("Bye")
         return True
 
     @staticmethod
     def do_reports(arg):
+        """Show reports.
+        Usage: reports <date> (Show reports of a given date.)
+        Usage: reports <date> <classroom> (Show reports of a given. Filter students by their classrooms.)"""
         args = arg.split()
         if len(args) == 0:
             print("Please supply arguments.")
@@ -58,6 +63,10 @@ class Shell(cmd.Cmd):
 
     @staticmethod
     def do_students(arg):
+        """Show or add new students.
+        Usage: students (Show all students.)
+        Usage: students <classroom> (Show all students. Filter students by their classrooms.)
+        Usage: students add <camera-id> (Add a new student to the database.)"""
         if len(arg.split()) == 2 and arg.split()[0] == 'add':
             commands.add_new_student(arg.split()[1])
         else:
@@ -83,6 +92,8 @@ class Shell(cmd.Cmd):
 
     @staticmethod
     def do_face(arg):
+        """Download a student's face and view.
+        Usage: face <student-id> (Download and view a student's face.)"""
         student_id = arg
         student = database.students_col.find_one({"id": student_id})
         if student is None:
@@ -97,7 +108,11 @@ class Shell(cmd.Cmd):
 
     @staticmethod
     def do_hours(arg):
-        if arg == "get":
+        """Show class hours.
+        Usage: hours (Show class hours.)"""
+        if arg == "set":
+            print("Not implemented.")
+        elif arg == "":
             hours = data.config["settings"]["hours"]
             table = {"Class": [], "Start": [], "Finish": []}
             for i in range(len(hours)):
@@ -106,18 +121,13 @@ class Shell(cmd.Cmd):
                 table["Start"].append(start)
                 table["Finish"].append(finish)
             print_table(table)
-        elif arg == "set":
-            print("Not implemented.")
         else:
             print("Please supply arguments.")
 
     @staticmethod
-    def complete_hours(text, line, begindx, endidx):
-        completions = ["get", "set"]
-        return [completion for completion in completions if completion.startswith(text)]
-
-    @staticmethod
     def do_cameras(arg):
+        """Show a list of connected cameras.
+        Usage: cameras (Show a list of connected cameras.)"""
         cameras = connection.get_cameras()
         table = {"ID": cameras['pi-id'], "Location": cameras["location"]}
         print_table(table)
